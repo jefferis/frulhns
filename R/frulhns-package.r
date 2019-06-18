eb6bc3211b58bb98260dadfe0ba699b86f9b7461
Gregory Jefferis <jefferis@gmail.com>
2019-06-18 13:44:03 +0100

    Auto package docs

--- R/frulhns-package.r
+++ R/frulhns-package.r
@@ -1,5 +1,9 @@
-#' frulhns
-#'
-#' @name frulhns
-#' @docType package
+#' @keywords internal
+#' @aliases frulhns
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
 NULL
